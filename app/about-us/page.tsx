8fae12ea3556b511919c148606ade256b0d5531a
chr0405 <ch0nn@naver.com>
2024-04-06 01:22:30 +0900

    hydrate

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -2,6 +2,16 @@
 import Navigation from "../../components/navigation"
 import Avatar from "./components/avatar"
 
+/*
+ server component 안에 client component를 가질 수 있다
+ 반면 client compoent 안에는 server component를 가질 수 없음
+*/
+
+/*
+ server component의 코드는 server에서만 실행되기 때문에
+ API key를 사용해서 API를 fetch한다고 하면
+ 해당 코드는 client로 가지 않기 때문에 보완에 신경쓰지 않아도 됨
+*/
 export default function AboutUs() {
     return (
         <div>
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,7 +1,29 @@
 "use client"
-// server에서 render 되는 거임
-// 모든 컴포넌트와 페이지들은 먼저 backend에서 render 된다
+/*
+ next.js
+ 1. backend에서 application을 pre-render
+    모든 component를 non interactive html로 바꾼다
+ 2. 프레임워크와 React.js를 innitialize
+ 3. "use client" 명령어를 가진 component가 hydrate(= interactive 됨)
+*/
+
 // hydration : 단순 html을 react application으로 초기화하는 작업
+
+/*
+server에서 render 되는 거임
+ 모든 컴포넌트와 페이지들은 먼저 backend에서 render 된다
+ "use client"를 사용하는 경우 그 후 frontend에서 hydrate 및 interactive 됨
+ 프레임워크에게 해당 component가 interactive하다는 것을 알려주기 때문
+ "use client"를 사용하지 않는 component는 server component로 간주
+ server component는 server에서 먼저 render 되고, hydrate 되진 않는다
+ 그럼 사용자가 다운로드 받을 js 양이 적어짐
+*/
+
+/*
+ client에서 hydrate되는 components는
+ = client에서 interative하게 만들어질 components는
+ "use client" 지시어를 맨 위에 갖고 있는 components들
+*/
 
 import Link from "next/link"
 import { usePathname } from "next/navigation"
