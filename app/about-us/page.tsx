1bd3c8116abcec8881da08247f3486dc38dc2708
chr0405 <ch0nn@naver.com>
2024-04-09 00:03:58 +0900

    layouts

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,5 +1,4 @@
 // 디렉토리 안에 꼭 page 파일을 생성해야 한다.
-import Navigation from "../../components/navigation"
 import Avatar from "./components/avatar"
 
 /*
@@ -17,7 +16,6 @@ export default function AboutUs() {
         <div>
             <h1>AboutUs!</h1>
             <Avatar />
-            <Navigation />
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 // import React from 'react'
 // import 할 필요가 없음.
 
-import Navigation from "../components/navigation";
 
 // 컴포넌트 이름은 상관 없음.
 // export default여야 함
@@ -12,7 +11,6 @@ export default function Page() {
     return (
         <div>
             <h1>hello!</h1>
-            <Navigation />
         </div>
     )
 }
