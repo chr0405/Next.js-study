e34badd40e38133e93876bbabaa827ee57e84210
chr0405 <ch0nn@naver.com>
2024-04-12 00:13:43 +0900

    error 페이지

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -2,8 +2,9 @@ import { API_URL } from "../app/(home)/page";
 
 async function getMovie(id : string) {
     await new Promise((resolve) => setTimeout(resolve, 5000));
-    const response = await fetch(`${API_URL}/${id}`);
-    return response.json();
+    throw new Error('something broken...');
+    // const response = await fetch(`${API_URL}/${id}`);
+    // return response.json();
 }
 
 export default async function MovieInfo({id} : {id : string}) {
